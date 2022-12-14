4853265a51e7290f1648f7da38e1a9dcf2f748a1
tiantiaf <tiantiaf@usc.edu>
2022-12-14 20:54:53 +0000

    fix a bug in model: len input

--- model/mm_models.py
+++ model/mm_models.py
@@ -141,30 +141,33 @@ class MMActionClassifier(nn.Module):
         # 2. Rnn forward
         # max pooling, time dim reduce by 8 times
         len_a = len_a//8
-        x_audio_packed_input = pack_padded_sequence(
-            x_audio, 
-            len_a.cpu().numpy(), 
-            batch_first=True, 
-            enforce_sorted=False
-        )
+        if len_a[0] != 0:
+            x_audio = pack_padded_sequence(
+                x_audio, 
+                len_a.cpu().numpy(), 
+                batch_first=True, 
+                enforce_sorted=False
+            )
+        if len_v[0] != 0:
+            x_video = pack_padded_sequence(
+                x_video, 
+                len_v.cpu().numpy(), 
+                batch_first=True, 
+                enforce_sorted=False
+            )
 
-        x_video_packed_input = pack_padded_sequence(
-            x_video, 
-            len_v.cpu().numpy(), 
-            batch_first=True, 
-            enforce_sorted=False
-        )
-
-        x_audio, _ = self.audio_rnn(x_audio_packed_input) 
-        x_video, _ = self.video_rnn(x_video_packed_input) 
-        x_audio, _ = pad_packed_sequence(   
-            x_audio, 
-            batch_first=True
-        )
-        x_video, _ = pad_packed_sequence(
-            x_video, 
-            batch_first=True
-        )
+        x_audio, _ = self.audio_rnn(x_audio) 
+        x_video, _ = self.video_rnn(x_video) 
+        if len_a[0] != 0:
+            x_audio, _ = pad_packed_sequence(   
+                x_audio, 
+                batch_first=True
+            )
+        if len_v[0] != 0:
+            x_video, _ = pad_packed_sequence(
+                x_video, 
+                batch_first=True
+            )
 
         # 3. Attention
         if self.en_att:
@@ -305,30 +308,33 @@ class SERClassifier(nn.Module):
         # 2. Rnn forward
         # max pooling, time dim reduce by 8 times
         len_a = len_a//8
-        x_audio_packed_input = pack_padded_sequence(
-            x_audio, 
-            len_a.cpu().numpy(), 
-            batch_first=True, 
-            enforce_sorted=False
-        )
+        if len_a[0] != 0:
+            x_audio = pack_padded_sequence(
+                x_audio, 
+                len_a.cpu().numpy(), 
+                batch_first=True, 
+                enforce_sorted=False
+            )
+        if len_t[0] != 0:
+            x_text = pack_padded_sequence(
+                x_text, 
+                len_t.cpu().numpy(), 
+                batch_first=True, 
+                enforce_sorted=False
+            )
 
-        x_text_packed_input = pack_padded_sequence(
-            x_text, 
-            len_t.cpu().numpy(), 
-            batch_first=True, 
-            enforce_sorted=False
-        )
-
-        x_audio, _ = self.audio_rnn(x_audio_packed_input) 
-        x_text, _ = self.text_rnn(x_text_packed_input) 
-        x_audio, _ = pad_packed_sequence(   
-            x_audio,
-            batch_first=True
-        )
-        x_text, _ = pad_packed_sequence(
-            x_text,
-            batch_first=True
-        )
+        x_audio, _ = self.audio_rnn(x_audio) 
+        x_text, _ = self.text_rnn(x_text)
+        if len_a[0] != 0:
+            x_audio, _ = pad_packed_sequence(   
+                x_audio,
+                batch_first=True
+            )
+        if len_t[0] != 0:
+            x_text, _ = pad_packed_sequence(
+                x_text,
+                batch_first=True
+            )
         
         # 3. Attention
         if self.en_att:
@@ -362,6 +368,91 @@ class SERClassifier(nn.Module):
         preds = self.classifier(x_mm)
         return preds, x_mm
 
+
+class ImageTextClassifier(nn.Module):
+    def __init__(
+        self, 
+        num_classes: int,       # Number of classes 
+        img_input_dim: int,     # Image data input dim
+        text_input_dim: int,    # Text data input dim
+        d_hid: int=64,          # Hidden Layer size
+        en_att: bool=False,     # Enable self attention or not
+        att_name: str=''        # Attention Name
+    ):
+        super(ImageTextClassifier, self).__init__()
+        self.dropout_p = 0.1
+        self.en_att = en_att
+        self.att_name = att_name
+        
+        # Projection head
+        self.img_proj = nn.Sequential(
+            nn.Linear(img_input_dim, d_hid),
+            nn.ReLU(),
+            nn.Dropout(self.dropout_p),
+            nn.Linear(d_hid, d_hid)
+        )
+            
+        # RNN module
+        self.text_rnn = nn.GRU(
+            input_size=text_input_dim, 
+            hidden_size=d_hid, 
+            num_layers=1, 
+            batch_first=True, 
+            dropout=self.dropout_p, 
+            bidirectional=False
+        )
+
+        # Self attention module
+        if self.att_name == "fuse_base":
+            self.fuse_att = FuseBaseSelfAttention(
+                d_hid=d_hid
+            )
+        
+        # classifier head
+        if self.en_att and self.att_name == "fuse_base":
+            self.classifier = nn.Sequential(
+                nn.Linear(d_hid, 64),
+                nn.ReLU(),
+                nn.Dropout(self.dropout_p),
+                nn.Linear(64, num_classes)
+            )
+            
+        self.init_weight()
+        
+    def init_weight(self):
+        for m in self._modules:
+            if type(m) == nn.Linear:
+                torch.nn.init.xavier_uniform(m.weight)
+                m.bias.data.fill_(0.01)
+            if type(m) == nn.Conv1d:
+                torch.nn.init.xavier_uniform(m.weight)
+                m.bias.data.fill_(0.01)
+
+    def forward(self, x_img, x_text, len_i, len_t):
+        # 1. img proj
+        x_img = self.img_proj(x_img[:, 0, :])
+        
+        # 2. Rnn forward
+        if len_t[0] != 0:
+            x_text = pack_padded_sequence(
+                x_text, 
+                len_t.cpu().numpy(), 
+                batch_first=True, 
+                enforce_sorted=False
+            )
+        x_text, _ = self.text_rnn(x_text)
+        if len_t[0] != 0:
+            x_text, _ = pad_packed_sequence(x_text, batch_first=True)
+        
+        # 3. Attention
+        if self.en_att:
+            if self.att_name == "fuse_base":
+                # get attention output
+                x_mm = torch.concat((x_img.unsqueeze(dim=1), x_text), dim=1)
+                x_mm = self.fuse_att(x_mm, len_i, len_t, 1)
+        # 4. MM embedding and predict
+        preds = self.classifier(x_mm)
+        return preds, x_mm
 
 class HARClassifier(nn.Module):
     def __init__(
@@ -472,6 +563,10 @@ class HARClassifier(nn.Module):
         x_acc, _ = self.acc_rnn(x_acc)
         x_gyro, _ = self.gyro_rnn(x_gyro)
 
+        # Length of the signal
+        l_a = l_a // 8
+        l_b = l_b // 8
+        
         # 3. Attention
         if self.en_att:
             if self.att_name == 'multihead':
@@ -487,7 +582,12 @@ class HARClassifier(nn.Module):
             elif self.att_name == "fuse_base":
                 # get attention output
                 x_mm = torch.concat((x_acc, x_gyro), dim=1)
-                x_mm = self.fuse_att(x_mm)
+                x_mm = self.fuse_att(
+                    x_mm, 
+                    val_a=l_a, 
+                    val_b=l_b, 
+                    a_len=x_acc.shape[1]
+                )
         else:
             # 4. Average pooling
             x_acc = torch.mean(x_acc, axis=1)
