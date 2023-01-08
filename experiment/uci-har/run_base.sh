87ff2834447087f5b7a071ca5364636a160d0925
tiantian feng <tiantiaf@usc.edu>
2023-01-08 15:11:21 -0800

    add bash for running

---
+++ experiment/uci-har/run_base.sh
@@ -0,0 +1,6 @@
+for alpha in 0.1 5.0; do
+   for fed_alg in fed_avg fed_opt fed_prox; do
+      taskset 100 python3 train.py --alpha $alpha --sample_rate 0.1 --learning_rate 0.05 --global_learning_rate 0.01 --num_epochs 200 --fed_alg $fed_alg --mu 0.01 --en_att --att_name fuse_base --hid_size 128
+   done
+done
+
